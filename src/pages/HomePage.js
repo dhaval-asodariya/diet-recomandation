f126dc3b98f38789b3fe045717865015561f381d
dhaval <asodariyadhaval1@gmail.com>
2024-06-29 19:50:09 +0530

    change

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,7 +9,7 @@ function HomePage() {
     <div style={{position:"relative",height:'660px',overflow:'hidden'}}>
         <img style={{width:'99vw',height:'700px'}} src={heroIng}></img>
         <div style={{position:'absolute',top:'100px',left:'100px'}}>
-        <h1 style={{fontSize:'60px'}}>Take Control Of <br/> Your Helth</h1> 
+        <h1 style={{fontSize:'60px',textAlign:'left'}}>Take Control Of <br/> Your Helth</h1> 
         <div style={{fontSize:'35px'}} >With Our Tailored Diet Plans</div>
         <Button onClick={()=>navigation('/login')} size='large' sx={{backgroundColor:'#7a0eb0',marginTop:'20px',padding:'15px 20px'}} variant="contained" color="success">
           Create Now!
